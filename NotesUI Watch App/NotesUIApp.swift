5595edbefaac3f45a4dc732986dd4a5cb4ea924d
Daniel Washington Ignacio <danielvertigo@hotmail.com>
2023-12-12 15:37:45 -0300

    Initial Commit

---
+++ NotesUI Watch App/NotesUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesUIApp.swift
+//  NotesUI Watch App
+//
+//  Created by Daniel Washington Ignacio on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesUI_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
